e1a252b9c28288ca9d58c0fafcf0d5d2d3cf0690
Paulo Neves <ptsneves@gmail.com>
2017-12-06 16:11:21 +0100

    Barebones NewsTagger building

--- news_tagger.py
+++ news_tagger.py
@@ -1,31 +1,39 @@
+#!/usr/bin/python3
+import nltk
 
 class NewsTagger(nltk.tag.api.TaggerI):
-  NewsTagger.FLOATING_POINT_REGEX = (r'[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?.', 'NUM')
+  FLOATING_POINT_REGEX = (r'[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?.', 'NUM')
 
-  def __init__(self, autosave=False):
+  def __init__(self):
     from nltk.corpus import brown
-    brownTaggedSents = brown.taggedSents(categories='news')
+    brown_tagged_sents = brown.tagged_sents()
 
-    self.tDefault = nltk.DefaultTagger('None')
-    self.tRegexp = nltk.RegexpTagger([FLOATING_POINT_REGEX], backoff = tDefault)
-    self.tBigram = nltk.BigramTagger(brownTaggedSents, backoff = tRegexp)
-    print(tBigram.tagSents(untaggedSentences))
+    self.t_default = nltk.DefaultTagger('None')
+    self.t_regexp = nltk.RegexpTagger([NewsTagger.FLOATING_POINT_REGEX], backoff = self.t_default)
+    self.t_bigram = nltk.BigramTagger(brown_tagged_sents, backoff = self.t_regexp)
 
-    self.autosave = autosave
-
-  def loadFromFile(newsTaggerPickleFile):
-    with open(newsTaggerPickleFile) as f:
+  def loadFromFile(news_tagger_pickle_file):
+    with open(news_tagger_pickle_file) as f:
       return pickle.load(f)
 
-  def saveToFile(self,fileName):
+  def saveToFile(self, fileName):
     with open(fileName, 'w') as f:
       pickle.dump(self)
 
-  def __enter__(self, autosave = True):
+  def __enter__(self, autosave = None):
     self.autosave = autosave
+    return self
 
-  def __exit__(self):
+  def __exit__(self, exc_type, exc_value, traceback):
     if self.autosave:
-      saveToFile(
-NewsTagger t(true)
+      self.saveToFile(self.autosave)
+
+  def tag_sents(self, sentences):
+    return self.t_bigram.tag_sents(tokens)
+
+  def tag(self, tokens):
+    return self.t_bigram.tag(tokens)
+
+with NewsTagger() as t:
+  print(t.tag(["Hello", "world"]))
 
