60d3a9a65d885862c2ed4cdcd42398c97100e5a6
Paulo Neves <ptsneves@gmail.com>
2017-12-07 17:02:29 +0100

    Added a download method to utils

--- utils.py
+++ utils.py
@@ -1,10 +1,49 @@
-import time
+#!/usr/bin/python3
 
 def appendTimestamToFileName(base_fn):
-    t = time.gmtime()
-    f_name = "{}-{}-{}-{}-{}-{}-{}".format(base_fn, t.tm_year, t.tm_mon,
-      t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec)
-    return f_name
+  import time
+  t = time.gmtime()
+  f_name = "{}-{}-{}-{}-{}-{}-{}".format(base_fn, t.tm_year, t.tm_mon,
+    t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec)
+  return f_name
+
+def download(url, output_fn = None):
+  import urllib.request
+  import os
+
+  def isSameFile(output_fn):
+    r = False
+    if output_fn and os.path.exists(output_fn):
+      file_size = int(urllib.request.urlopen(url).info().get('Content-Length', -1))
+      r = file_size == os.path.getsize(output_fn)
+
+    return r
+
+  #https://stackoverflow.com/a/13895723/227990
+  def reporthook(blocknum, blocksize, totalsize):
+    import sys
+    readsofar = blocknum * blocksize
+    if totalsize > 0:
+        percent = readsofar * 1e2 / totalsize
+        s = "\r%s %5.1f%% %*d / %d" % (
+            url, percent, len(str(totalsize)), readsofar, totalsize)
+        sys.stderr.write(s)
+        if readsofar >= totalsize: # near the end
+            sys.stderr.write("\n")
+    else: # total size is unknown
+        sys.stderr.write("read %d\n" % (readsofar,))
+
+  if isSameFile(output_fn):
+    print("{} already exists. Download skipping".format(output_fn))
+    return
+
+  ret_fn, headers = urllib.request.urlretrieve(url, output_fn, reporthook)
+
+  if not output_fn:
+    with open(ret_fn, "r") as f:
+      r = f.read()
+    os.remove(ret_fn)
+    return r
 
 from html.parser import HTMLParser
 #https://stackoverflow.com/questions/753052/strip-html-from-strings-in-pythoni
@@ -33,4 +72,3 @@ def untagSentences(tagged_sentences):
     untagged_sentences.append(nltk.tag.util.untag(s))
 
   return untagged_sentences
-
